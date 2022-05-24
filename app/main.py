c9b94a5cd00fd579b6a8cf66cb3e2b3cd05bd000
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-05-24 16:06:51 -0700

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+"""
+JUST MERCY TEXT APP
+
+This application sends and receives texts
+from participants to support scheduling for the
+Just Merc project
+
+Ian Richard Ferguson | Stanford University
+"""
+
+# --- Imports
+from flask import Flask, render_template, request, redirect, url_for
+from helper import *
+import os
+
+
+# --- App init + routing
+app = Flask(__name__)
+system_path = os.path.join(".")
+
+print("\n== App Running ==\n")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/stream", methods=["GET", "POST"])
+def stream():
+
+    data = stream_to_dataframe()
+
+    return render_template("stream.html", data=data.to_html())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
