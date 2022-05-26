cc3c81441351cc7824ca43e39da1fbd215cb4e3e
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-05-26 12:13:18 -0700

    additional routing

--- app/main.py
+++ app/main.py
@@ -27,7 +27,8 @@ print("\n== App Running ==\n")
 
 
 
-# --- Routing
+# ==== Routing ====
+# --- Index
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -46,15 +47,39 @@ def texts():
     return render_template("texts.html")
 
 
-@app.route("/stream", methods=["GET", "POST"])
-def stream():
+# --- Logs
+@app.route("/participant_log", methods=["GET", "POST"])
+def participant_log():
     data = db_to_dataframe()
-
     return render_template("stream.html", data=data.to_html())
 
 
+@app.route("/outgoing_texts", methods=["GET", "POST"])
+def outgoing_texts():
+    data = get_texts(sent_by_me=True)
+    return render_template("outgoing_texts.html", data=data.to_html())
+
+
+@app.route("/incoming_texts", methods=["GET", "POST"])
+def incoming_texts():
+    data = get_texts(sent_by_me=False)
+    return render_template("incoming_texts.html", data=data.to_html())
+
+
+
+# --- Utilities
 @app.route("/update", methods=["GET", "POST"])
 def update():
+
+    if request.method == "POST":
+        sub_name = request.form["ignore_name"]
+        sub_number = request.form["ignore_number"]
+
+        ignore_participant(full_name=sub_name,
+                           contact_number=sub_number)
+
+        return redirect(url_for('index'))
+        
     return render_template("update.html")
 
 
